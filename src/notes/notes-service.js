f07545f9b7d8dd06d395188568a8b8e079e9fd9b
Courtney Carson <cmacarson@gmail.com>
2020-11-12 09:43:44 -0800

    finish notes-server

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -3,18 +3,41 @@ const NotesService = {
     console.log(db)
     return db.select('*').from('notes')
   },
+  getNoteById(db, note_id) {
+    return db
+      .from('notes')
+      .select('*')
+      .where({ 'id': note_id })
+      .first()
+  },
+  insertNote(db, newNote) {
+    return db
+      .insert(newNote)
+      .into('notes')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  deleteNote(db, note_id) {
+    return db('notes')
+      .where({ 'id': note_id })
+      .delete()
+  }, 
+  updateNote(db, note_id, newNote) {
+    return db('notes')
+      .where({ 'id': note_id })
+      .update(newNote, returning = true)
+    .returning('*')
+  },
   serializeNote(note) {
     const { content, title, date_created } = note
     return {
       content: content,
-      title: title, 
+      title: title,
       date_created: date_created
     }
   }
-
 }
-
-
-
 
 module.exports = NotesService;
