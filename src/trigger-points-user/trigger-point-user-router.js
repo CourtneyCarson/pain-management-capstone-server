88c9183fff1699a013caefc568a91c009183d641
Courtney Carson <cmacarson@gmail.com>
2020-11-25 18:05:21 -0800

    remove ability to save duplicate trigger points

--- src/trigger-points-user/trigger-point-user-router.js
+++ src/trigger-points-user/trigger-point-user-router.js
@@ -1,51 +1,79 @@
-const express = require('express')
-const xss = require('xss')
-const path = require('path')
-const requireAuth = require('../middleware/jwt-auth')
+const express = require('express');
+const xss = require('xss');
+const path = require('path');
+const requireAuth = require('../middleware/jwt-auth');
 
-const TriggerPointUserService = require('./trigger-point-user-service')
+const TriggerPointUserService = require('./trigger-point-user-service');
 
-const triggerPointUserRouter = express.Router()
-const jsonParser = express.json()
+const triggerPointUserRouter = express.Router();
+const jsonParser = express.json();
 
 triggerPointUserRouter
   .route('/')
   .get(requireAuth, (req, res, next) => {
-    console.log(req.user)
-    console.log(req.user.id)
+    console.log(req.user);
+    console.log(req.user.id);
     TriggerPointUserService.getAllTriggerPoints(
       req.app.get('db'),
       req.user.id
     )
       .then(user_tp => {
-        console.log(user_tp)
+        console.log(user_tp);
         if (!user_tp) {
           return res.status(404).json({
             error: { message: `Users Trigger Points do not exist` }
-          })
+          });
         }
-        res.json(user_tp)
+        res.json(user_tp);
       })
-      .catch(next)
+      .catch(next);
   })
 
   .post(requireAuth, jsonParser, (req, res, next) => {
-    console.log(req.body)
-    const { trigger_points_id } = req.body
-    const newTp = { user_id: req.user.id, trigger_points_id }
+    console.log(req.body);
+    const { trigger_points_id } = req.body;
+    const newTp = { user_id: req.user.id, trigger_points_id };
 
-    TriggerPointUserService.insertTriggerPoints(
+    TriggerPointUserService.getTriggerPointsByUser(
       req.app.get('db'),
-      newTp
+      req.user.id
     )
-      .then(tp => {
-        res
-          //display the 201 status code
-          .status(201)
-          .json(tp)
+      .then(showTriggerPointByUserId => {
+        // console.log('show tp by id', showTriggerPointByUserId);
+        // define variable to store duplicates 
+        let foundDuplicate = 0;
+        // loop through array of objects from the response
+        for (let i = 0; i < showTriggerPointByUserId.length; i++) {
+          //if response contains trigger_points_id 
+          if (showTriggerPointByUserId[i].trigger_points_id == trigger_points_id) {
+            // update found duplicate 
+            foundDuplicate = 1;
+          }
+        }
+      
+
+        if (foundDuplicate == 0) {
+          // check if user already has trigger point id saved 
+          TriggerPointUserService.insertTriggerPoints(
+            req.app.get('db'),
+            newTp
+          )
+            .then(tp => {
+              res
+                //display the 201 status code
+                .status(201)
+                .json(tp);
+            })
+            .catch(next);
+        } else {
+          res
+          //display the 400 status code
+          .status(400)
+            .json({ error: "Duplicated entry" })
+        }
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
 
 /// routes by id
@@ -55,7 +83,7 @@ triggerPointUserRouter
     if (isNaN(parseInt(req.params.tp_id))) {
       return res.status(400).json({
         error: { message: `Invalid id ${req.params.tp_id}` }
-      })
+      });
     }
     TriggerPointUserService.getTriggerPointsById(
       req.app.get('db'),
@@ -65,20 +93,20 @@ triggerPointUserRouter
         if (!tp) {
           return res.status(404).json({
             error: { message: `Trigger Point does not exist` }
-          })
+          });
         }
-        console.log(tp)
-        res.triggerpoint = tp
-        next()
+        console.log(tp);
+        res.triggerpoint = tp;
+        next();
       })
-      .catch(next)
+      .catch(next);
   })
 
   //get trigger points by id
   .get((req, res) => {
-    res.json(res.triggerpoint)
-  })
+    res.json(res.triggerpoint);
+  });
 
 
 
-module.exports = triggerPointUserRouter
+module.exports = triggerPointUserRouter;
