b7dd8a4ca0dccf659996e5173e52dbd31353d68a
Courtney Carson <cmacarson@gmail.com>
2023-10-19 18:15:13 -0700

    update db url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://courtneycarson@localhost/trigger_point',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://courtneycarson@localhost/trigger_point-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://tttymgna:jo-2d6oLFX7O_ArhPa6CcXNXzFK3chrH@suleiman.db.elephantsql.com/tttymgna',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://tttymgna:jo-2d6oLFX7O_ArhPa6CcXNXzFK3chrH@suleiman.db.elephantsql.com/tttymgna-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
 }
